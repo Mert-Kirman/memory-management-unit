30f8f8d7387abb5b5dc44ce1fea77bd6325aa82c
Mert Kirman <mertkirman@Merts-MacBook-Air.local>
2024-05-12 23:06:19 +0300

    Function 1 working

---
+++ main.rkt
@@ -0,0 +1,29 @@
+;name surname
+;student id
+;compiling: yes
+;complete: yes
+#lang racket
+
+(provide (all-defined-out))
+
+
+(define (binary_to_decimal binary)
+  (if (= (string-length binary) 1)
+      (string->number binary)
+      (+ (string->number (string (string-ref binary (- (string-length binary) 1)))) (* 2 (binary_to_decimal (substring binary 0 (- (string-length binary) 1)))))))
+
+;(define (relocator args limit base))
+
+;(define (divide_address_space num page_size))
+
+;(define (page args page_table  page_size))
+
+;(define (find_sin value num))
+
+;(define (myhash arg table_size))
+
+;(define (hashed_page arg table_size page_table page_size))
+
+;(define (split_addresses args size))
+
+;(define (map_addresses args page_table page_size))
